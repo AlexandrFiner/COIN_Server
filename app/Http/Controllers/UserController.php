7fd7e27542453309ba8489993ae538e568c96d91
finer <finersanya@gmail.com>
2021-07-18 02:32:55 +0500

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        // Создание пользователя
+
+        try {
+            $user = User::create([
+                'login' => $request['login'],
+                'password' => $request['password'],
+                'provider' => $request['provider'],
+                'api_token' => Str::random(60)
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => [
+                    'error_code' => 100,
+                    'error_message' => $e
+                ]
+            ], 403);
+        }
+
+        return response()->json([
+            "response" => $user
+        ], 200);
+    }
+
+    public function get(Request $request) {
+        // Получение информации о пользователе
+
+        try {
+            $user = User::findOrFail($request['id']);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => [
+                    'error_code' => 404,
+                    'error_message' => $e
+                ]
+            ], 404);
+        }
+
+        return response()->json([
+            "response" => $user
+        ], 200);
+    }
+
+    public function auth(Request $request) {
+
+    }
+}
