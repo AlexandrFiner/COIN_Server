ef0a9699bc70cc4259e625e42faf7b9cba17f745
finer <finersanya@gmail.com>
2021-07-28 19:14:09 +0500

    Фабрика

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,25 +23,21 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'login' => rand(1, 171674556),
+            'password' => Str::random(10),
+            'api_token' => Str::random(10),
+            'provider' => 'vk',
+            'balance_coin' => rand(0, 100),
+            'mining_speed' => rand(0.1, 1.0),
+            'group_vk' => 0,
+            'clan_id' => 0
+            /*
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            */
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
